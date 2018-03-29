d29cebfc6082199d99602a4a81f91d7ad17c3768
Chris <orthagonal1@gmail.com>
2018-03-29 16:37:46 -0500

    fix server inject

--- helpers/replaceServerInject.js
+++ helpers/replaceServerInject.js
@@ -4,6 +4,8 @@ module.exports = function replaceServerInject(p) {
   // get name:
   const param = p.value.arguments[1].params[0].name;
   const call = codeshift.callExpression(codeshift.identifier('inject'), [p.value.arguments[0]]);
+  //todo: make 'server' work for server2, testServer, etc
+  call.callee = codeshift.memberExpression(codeshift.identifier('server'), codeshift.identifier('inject'));
   const awaitExpr = codeshift.awaitExpression(call);
   const response = codeshift.variableDeclaration(
     'const',
